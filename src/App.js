4171a78a575fff63a8c2645d5fe0e59e95771e8e
Andrew McMillion <amcmillion3@gmail.com>
2022-03-31 21:58:15 -0400

    Setup Project Components and Project Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { projectData as data } from './assets/project-data';
+import Layout from './Components/Layout';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+        <Route path='/' element={<Layout data={ data }/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
