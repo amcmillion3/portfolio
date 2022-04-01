9bf65638613c3a73553c5db84640ed7d6dd48184
Andrew McMillion <amcmillion3@gmail.com>
2022-04-01 05:50:17 -0400

    Update file to React 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById("root");
+ReactDOMClient.createRoot(rootElement).render(
+  <App />
 );
