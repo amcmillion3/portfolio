cbdbe06eac5adfcb567d61b00ab58f24fefa717f
Andrew McMillion <amcmillion3@gmail.com>
2022-06-27 22:03:12 -0400

    Add screenshots for project links

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+const images = importAll(require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/));
+
 function ProjectCard(props) {
+    const link = props.image;
+
     return(
         <div key={props.title} className="project-card">
             <Link to={`/projects/${props.title}`}>
-                <img className="project-image" src={`${props.image}`} alt={`${props.title}`} />
+                <img className="project-image" src={`${images[link]}`} alt={`${props.title}`} />
             </Link>
         </div>
     )
